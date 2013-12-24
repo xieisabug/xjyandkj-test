309f811cf5894ada830b1aa9ad3ad7a745f661ed
xjyaitf@gmail.com <xjyaitf@gmail.com@8f6a77e5-1006-d899-27da-8f5107530c78>
2013-12-24 06:39:28 +0000

    对聊天的后台的初步搭建

---
+++ trunk/WebSocketTest/SpringMVCWebsocket/src/main/java/com/kjtest/controller/WebSocketConfig.java
@@ -0,0 +1,31 @@
+package com.kjtest.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/chat");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration channelRegistration) {
+    }
+
+    @Override
+    public void configureClientOutboundChannel(ChannelRegistration channelRegistration) {
+    }
+}
