f7b83f399f348137488e8caf30b2704c527eee14
xjyaitf@gmail.com <xjyaitf@gmail.com@8f6a77e5-1006-d899-27da-8f5107530c78>
2013-12-24 09:12:56 +0000

    动画更新

--- trunk/MooToolsTest/panel/index.js
+++ trunk/MooToolsTest/panel/index.js
@@ -5,8 +5,8 @@ var Panel = new Class({
             panelTitleCss : 'panel-head',
             panelTitleIcon : 'default',
             panelContentCss : 'panel-content',
-            contentHeight:'auto',
-            contentWidth:'auto'
+            titleWidth:200,
+            contentHeight:200
         },option);
         this.panel = el;
         var childrens = el.getChildren();
@@ -23,9 +23,10 @@ var Panel = new Class({
         }
 
         this.panelTitle.addClass(this.option.panelTitleCss);
+        this.panelTitle.setStyle('width',this.option.titleWidth);
         this.panelContent.addClass(this.option.panelContentCss);
         this.panelContent.setStyles({
-            width:this.option.contentWidth,
+            width:this.option.titleWidth-26,
             height:this.option.contentHeight
         });
         return this;
@@ -45,5 +46,29 @@ var Panel = new Class({
     removePanelContentCss : function(className) {
         this.panelContent.removeClass(className);
         return this;
+    },
+    updateWithAnimate:function(type,html){
+        var thiz = this;
+        var fx = new Fx.Morph(thiz.panelContent, {
+            duration: 1000,
+            transition:Fx.Transitions['Quad']['easeIn']
+        });
+        if(type == 'toggle') {
+            var h = thiz.panelContent.getSize().y-27;
+            fx.start({
+                height:0
+            }).chain(function(){
+                thiz.panelContent.set('html',html);
+                fx.start({
+                    height:h
+                });
+            });
+        } else if(type == 'append') {
+            new Element('div').set('html',html).getChildren().each(function(item){
+                thiz.panelContent.grab(item);
+            });
+            var h = thiz.panelContent.getScrollSize().y-27;
+            fx.start({height:h});
+        }
     }
 });
